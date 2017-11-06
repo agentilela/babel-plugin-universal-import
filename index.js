fffb07f5f33ab3de8ba5537cde8e564072b3e4de
Alex Gentile <agentilela@gmail.com>
2017-11-06 12:29:37 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use-strict'
 
+const {addImport, addComment} = require('@babel/helper-module-imports')
+
 module.exports = function ({ types: t, template }) {
   const visited = Symbol('visited')
   const universalImportId = Symbol('universalImportId')
